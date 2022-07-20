bc8c96d16d73624d05c2d2bf91f9df48f3b27e16
Otto <zzy801997@163.com>
2022-07-20 23:14:54 +1000

    refactor code and modify README

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # listary_flomo_script
+
+因为没研究明白listary的插件怎么写，于是乎曲线救国决定，用python完成脚本并打包成exe文件，用listary运行exe文件实现功能。
+
+Because I cannot figure out how to write listary plugin, I decided to use python to write the script and package it as an exe file, and use listary to run the exe file to implement the function.
+
+# 使用方法 Usage
+
+## 脚本 Script
+
+```
+git clone https://github.com/zhanggzzy/listary_flomo_script.git
+cd listary_flomo_script
+python main.py
+```
+
+首次使用会提示输入 flomo API 的 url（需要flomo pro）
+```
+Enter your Flomo API URL:
+```
+
+之后会在同目录生成一个txt记录该url，之后使用无需再次输入。
+
+此时可以使用命令行测试脚本
+```
+python main.py <input text>
+```
+
+如果成功，会输出结果
+```
+Successfully Post to Flomo
+```
+
+如果失败，会输出错误信息
+```
+Failed to Post to Flomo
+```
+
+## Listary
+
+使用pyinstaller生成exe文件。
+
+```
+pyinstaller -F main.py
+```
+
+然后在 listary 设置中配置。在 选项-特性-命令 新增一个命令：
+
+- 关键字：flomo
+- 标题：发布flomo MEMO
+- 路径：<脚本exe文件所在路径>
+- 参数："{query}"
+
+然后勾选静默执行
+
+便可以在 listary 中使用该命令。
--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import sys
-from urllib import response
 import requests
-from privacy import flomo_api
 
+# Get the content sent to the server from the command line
 content = ' '.join(sys.argv[1:])
 
+# get url settings
+try:
+    # if there is already a url in the content, use it
+    with open('url.txt', 'r') as f:
+        url = f.read()
+except FileNotFoundError:
+    # if there is no url in the content, ask the user for one (for the first time)
+    with open('url.txt', 'w') as f:
+        url = input('Enter your Flomo API URL: ')
+        f.write(url)
+
+# if there is no content, terminate the program
 if not content:
     print("Please enter some text to send to Flomo")
     exit(1)
+    
 
-url = flomo_api
-
+# set the headers and payload for the request
 header = {'Content-Type': 'application/json'}
 body = {'content': content}
 
+# send the request and get the response
 response = requests.post(url, headers=header, json=body)
 
+# print the response, which will not be shown in listary
 if response.status_code == 200:
     print("Successfully Post to Flomo")
 else:
